c765e1678b3222b3ac4c3de4a0b744d45492de30
ssav7912 <ssav7912@uni.sydney.edu.au>
2022-09-06 11:35:05 +1000

    Added url flag, gpio support

--- board/board.py
+++ board/board.py
@@ -1,8 +1,9 @@
-# import pigpio
+import pigpio
 import subprocess
 import asyncio
 import requests
 from core.STScorelib import Resident, Location
+import argparse
 
 
 class Board:
@@ -10,15 +11,54 @@ class Board:
     Board controller class.
     '''
     manifest: 'set[str]'
-    residents: 'set[Resident]'
+    residents: 'list[Resident]'
     url: str
 
 
     def __init__(self, url):
-        # self.io = pigpio.pi()
-        self.residents: 'set[Resident]' = set()
+        self.io = pigpio.pi()
+        self.pioinit([2, 3])
+        self.residents: 'set[Resident]' = []
         self.manifest = set()
         self.url = url
+        self.mapping = self.initpinstruct()
+        
+
+
+    #Pin mapping:
+    #Resident 1:
+    #   - HOME -> 2
+    #   - WORK ->
+    #Resident 2:
+    #    - HOME -> 3
+    def initpinstruct(self) {
+        # mapping = {resident: {Location.HOME:None, Location.SHOPS: None, Location.WORK: None, Location.GYM: None} for resident in self.residents}
+        mapping = {
+            1: {Location.HOME:2, Location.SHOPS: None, Location.WORK: None, Location.GYM: None},
+            2: {Location.HOME:3, Location.SHOPS: None, Location.WORK: None, Location.GYM: None}
+        }
+        return mapping
+
+    }
+
+    def pioinit(self, outputpins: 'list[int]') -> None:
+        self.outputpins = outputpins
+        
+        for pin in outputpins:
+            self.io.set_mode(pin, pigpio.OUTPUT)
+            self.io.write(pin, 0)
+
+    def move_location(self, resident, newLocation: 'Location') -> None:
+        oldpin = self.mapping[self.residents.index(resident)][resident.location]
+
+        self.io.write(oldpin, 0)
+
+        resident.location = newLocation
+
+        newpin = self.mapping[self.residents.index(resident)][resident.location]
+        
+        self.io.write(newpin, 1)
+
 
     '''
     Sends a GET to the info server to get the resident manifest.
@@ -30,16 +70,17 @@ class Board:
         updates = list(ids)
         locations = getLocations(updates)
 
-        for id, location in zip(updates, locations):
+        for i, id, location in enumerate(zip(updates, locations)):
             if id not in self.manifest:
                 self.manifest.add(id)
                 resident = Resident(id, "placeholder")
-                resident.location = Location.fromString(location)
-                self.residents.add(resident)
+                self.move_location(self.getResidentById(id), Location.fromString(location))
+                self.residents.append(resident)
 
             elif id in self.manifest:
                 resident = self.getResidentById(id)
-                resident.location = Location.fromString(location)
+                
+                self.move_location(self.getResidentById(id), Location.fromString(location))
                 
 
         self.manifest.update(ids)
@@ -85,8 +126,17 @@ def getLocations(ids: 'list[str]') -> 'list[Location]':
 if __name__ == "__main__":
     # subprocess.Popen("sudo pigpiod")
         
+
+    parser = argparse.ArgumentParser(description="board.py [URL] --pins [pins]")
+
+    parser.add_argument('URL', type=str, help="Required URL to receive resident data from")
+
+
+    args = parser.parse_args()
+
+
     
-    board = Board(URL)
+    board = Board(arg.pos_arg[0])
 
     board.AskForUpdate()
     print(board.residents)
